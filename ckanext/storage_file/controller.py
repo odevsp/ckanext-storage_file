814da6b9c1f0078797f845d50aef5b854c5eed34
odevsp <rsaliner@everis.com>
2016-03-21 11:02:51 +0100

    Security

--- ckanext/storage_file/controller.py
+++ ckanext/storage_file/controller.py
@@ -1,8 +1,14 @@
 from logging import getLogger
-from ckan.lib.base import request, BaseController, abort, json
+from ckan.lib.base import request, BaseController, abort, json, c
+from ckan.plugins import toolkit
 
+import ckan.logic as logic
+import ckan.plugins as p
 import json
 import pylons.config as config
+import ckan.model as model
+import ckan.logic
+
 
 log = getLogger(__name__)
 
@@ -25,15 +31,24 @@ class Resource_fileController(BaseController):
 
 	def change_persisted_file(self):
 
+		if not request.method == 'POST':
+			abort(405, 'Method not allowed')
+
+		if not c.userobj:
+			abort(401, 'Forbidden, need user login')
+
+		if not c.userobj.sysadmin:
+			abort(401, 'Forbidden, need API-Key')
+
 		if not request.body:
-			abort(405, 'No data POST')
+			abort(400, 'No body data received')
 
 		content_type = request.headers.get('Content-Type', '')
 
 		if not content_type.startswith('application/json'):
 			abort(415, 'Content-Type should be "application/json"')
 
-		received_data=request.body;
+		received_data=request.body
 
 		file_route = config.get('ckan.storage_file.route_file') 
 
@@ -43,4 +58,3 @@ class Resource_fileController(BaseController):
 			self.create_file(file_route,received_data)
 		else:
 			abort(400, 'Wrong Syntax')
-		
